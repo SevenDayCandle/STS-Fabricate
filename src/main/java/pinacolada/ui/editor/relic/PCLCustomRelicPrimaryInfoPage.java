dc33a783ef65811f68b13940b97d94743a6d3020
Pina Colada <85974823+SevenDayCandle@users.noreply.github.com>
2025-01-03 10:31:27 -0800

    - Fix image paths - Add loadout option to relics

--- src/main/java/pinacolada/ui/editor/relic/PCLCustomRelicPrimaryInfoPage.java
+++ src/main/java/pinacolada/ui/editor/relic/PCLCustomRelicPrimaryInfoPage.java
@@ -18,21 +18,30 @@ import extendedui.ui.tooltips.EUITourTooltip;
 import extendedui.utilities.EUIFontHelper;
 import org.apache.commons.lang3.StringUtils;
 import pinacolada.cards.base.PCLCard;
+import pinacolada.effects.screen.PCLCustomDeletionConfirmationEffect;
+import pinacolada.effects.screen.PCLCustomLoadoutEditEffect;
 import pinacolada.relics.PCLCustomRelicSlot;
 import pinacolada.relics.PCLDynamicRelicData;
 import pinacolada.relics.PCLRelicData;
 import pinacolada.resources.PGR;
+import pinacolada.resources.loadout.PCLCustomLoadout;
+import pinacolada.resources.loadout.PCLCustomLoadoutInfo;
+import pinacolada.resources.loadout.PCLLoadout;
 import pinacolada.resources.pcl.PCLCoreImages;
 import pinacolada.skills.PSkill;
 import pinacolada.ui.PCLValueEditor;
 import pinacolada.ui.editor.PCLCustomEditEntityScreen;
 import pinacolada.ui.editor.PCLCustomGenericPage;
+import pinacolada.ui.editor.PCLCustomLoadoutDialog;
 
 import java.util.Arrays;
 import java.util.Collections;
 
 public class PCLCustomRelicPrimaryInfoPage extends PCLCustomGenericPage {
     protected PCLCustomRelicEditScreen effect;
+    protected EUIButton addLoadoutButton;
+    protected EUIButton editLoadoutButton;
+    protected EUIButton deleteLoadoutButton;
     protected EUIButton resetLoadoutValue;
     protected EUILabel header;
     protected EUITextBoxInput idInput;
@@ -41,6 +50,7 @@ public class PCLCustomRelicPrimaryInfoPage extends PCLCustomGenericPage {
     protected EUISearchableDropdown<AbstractRelic> replacementDropdown;
     protected EUIDropdown<AbstractRelic.RelicTier> tierDropdown;
     protected EUIDropdown<AbstractRelic.LandingSound> sfxDropdown;
+    protected EUIDropdown<PCLLoadout> loadoutDropdown;
     protected EUILabel idWarning;
     protected PCLValueEditor maxUpgrades;
     protected PCLValueEditor branchUpgrades;
@@ -117,7 +127,29 @@ public class PCLCustomRelicPrimaryInfoPage extends PCLCustomGenericPage {
                 .setCanAutosizeButton(true)
                 .setItems(AbstractRelic.LandingSound.values())
                 .setTooltip(EUIRM.strings.relic_landingSound, PGR.core.strings.cetut_landingSound);
-        replacementDropdown = (EUISearchableDropdown<AbstractRelic>) new EUISearchableDropdown<AbstractRelic>(new EUIHitbox(START_X, screenH(0.51f), MENU_WIDTH, MENU_HEIGHT), relic -> relic.name)
+
+        loadoutDropdown = new EUISearchableDropdown<PCLLoadout>(new EUIHitbox(START_X, screenH(0.52f), MENU_WIDTH, MENU_HEIGHT), (item -> {
+            String res = item.getName();
+            return StringUtils.isEmpty(res) ? EUIRM.strings.ui_na : res;
+        }))
+                .setOnChange(selectedSeries -> {
+                    setLoadout(!selectedSeries.isEmpty() ? selectedSeries.get(0) : null);
+                })
+                .setHeader(FontHelper.topPanelAmountFont, 0.8f, Settings.GOLD_COLOR, PGR.core.strings.sui_seriesUI)
+                .setCanAutosizeButton(true)
+                .setShowClearForSingle(true)
+                .setTooltip(PGR.core.strings.sui_seriesUI, "");
+        addLoadoutButton = new EUIButton(EUIRM.images.plus.texture(), new EUIHitbox(START_X, screenH(0.52f), MENU_HEIGHT * 0.75f, MENU_HEIGHT * 0.75f))
+                .setOnClick(() -> this.openLoadoutCreator(PGR.core.strings.cedit_newLoadout, null))
+                .setTooltip(PGR.core.strings.cedit_newLoadout, "");
+        editLoadoutButton = new EUIButton(PCLCoreImages.Menu.edit.texture(), new EUIHitbox(START_X, screenH(0.52f), MENU_HEIGHT * 0.75f, MENU_HEIGHT * 0.75f))
+                .setOnClick(() -> this.openLoadoutCreator(PGR.core.strings.cedit_renameItem, EUIUtils.safeCast(effect.getBuilder().loadout, PCLCustomLoadout.class)))
+                .setTooltip(PGR.core.strings.cedit_renameItem, "");
+        deleteLoadoutButton = new EUIButton(PCLCoreImages.Menu.delete.texture(), new EUIHitbox(START_X, screenH(0.52f), MENU_HEIGHT * 0.75f, MENU_HEIGHT * 0.75f))
+                .setOnClick(() -> this.openLoadoutDelete(EUIUtils.safeCast(effect.getBuilder().loadout, PCLCustomLoadout.class)))
+                .setTooltip(PGR.core.strings.cedit_deleteItem, "");
+
+        replacementDropdown = (EUISearchableDropdown<AbstractRelic>) new EUISearchableDropdown<AbstractRelic>(new EUIHitbox(START_X, screenH(0.42f), MENU_WIDTH, MENU_HEIGHT), relic -> relic.name)
                 .setOnChange(selectedSeries -> {
                     effect.modifyAllBuilders((e, i) -> e.setReplacementIDs(EUIUtils.arrayMapAsNonnull(selectedSeries, String.class, s -> s.relicId)));
                 })
@@ -125,28 +157,31 @@ public class PCLCustomRelicPrimaryInfoPage extends PCLCustomGenericPage {
                 .setCanAutosizeButton(true)
                 .setIsMultiSelect(true)
                 .setTooltip(PGR.core.strings.cedit_relicReplace, PGR.core.strings.cetut_relicReplace);
-        maxUpgrades = new PCLValueEditor(new EUIHitbox(screenW(0.262f), screenH(0.4f), MENU_WIDTH / 4, MENU_HEIGHT)
+        maxUpgrades = new PCLValueEditor(new EUIHitbox(screenW(0.262f), screenH(0.32f), MENU_WIDTH / 4, MENU_HEIGHT)
                 , PGR.core.strings.cedit_maxUpgrades, this::modifyMaxUpgrades)
                 .setLimits(-1, PSkill.DEFAULT_MAX)
                 .setTooltip(PGR.core.strings.cedit_maxUpgrades, PGR.core.strings.cetut_maxUpgrades)
                 .setHasInfinite(true, true);
-        branchUpgrades = new PCLValueEditor(new EUIHitbox(screenW(0.362f), screenH(0.4f), MENU_WIDTH / 4, MENU_HEIGHT)
+        branchUpgrades = new PCLValueEditor(new EUIHitbox(screenW(0.362f), screenH(0.32f), MENU_WIDTH / 4, MENU_HEIGHT)
                 , PGR.core.strings.cedit_branchUpgrade, (val) -> effect.modifyAllBuilders((e, i) -> e.setBranchFactor(val)))
                 .setLimits(0, PSkill.DEFAULT_MAX)
                 .setTooltip(PGR.core.strings.cedit_branchUpgrade, PGR.core.strings.cetut_branchUpgrade)
                 .setHasInfinite(true, true);
-        loadoutValue = new PCLValueEditor(new EUIHitbox(screenW(0.462f), screenH(0.4f), MENU_WIDTH / 4, MENU_HEIGHT)
+        loadoutValue = new PCLValueEditor(new EUIHitbox(screenW(0.462f), screenH(0.32f), MENU_WIDTH / 4, MENU_HEIGHT)
                 , PGR.core.strings.cedit_loadoutValue, (val) -> effect.modifyAllBuilders((e, i) -> e.setLoadoutValue(val)))
                 .setLimits(0, PSkill.DEFAULT_MAX)
                 .setTooltip(PGR.core.strings.cedit_loadoutValue, PGR.core.strings.cetut_loadoutValue)
                 .setHasInfinite(true, true);
-        resetLoadoutValue = new EUIButton(PCLCoreImages.Core.backArrow.texture(), new EUIHitbox(screenW(0.502f), screenH(0.403f), MENU_HEIGHT * 0.75f, MENU_HEIGHT * 0.75f))
+        resetLoadoutValue = new EUIButton(PCLCoreImages.Core.backArrow.texture(), new EUIHitbox(screenW(0.502f), screenH(0.323f), MENU_HEIGHT * 0.75f, MENU_HEIGHT * 0.75f))
                 .setOnClick(() -> {
                     int val = PCLRelicData.getValueForRarity(effect.getBuilder().tier);
                     loadoutValue.setValue(val, true);
                 })
                 .setTooltip(PGR.core.strings.loadout_reset, "");
 
+        editLoadoutButton.setActive(false);
+        deleteLoadoutButton.setActive(false);
+        refreshLoadoutItems();
         refresh();
     }
 
@@ -186,6 +221,27 @@ public class PCLCustomRelicPrimaryInfoPage extends PCLCustomGenericPage {
         }
     }
 
+    protected void openLoadoutCreator(String title, PCLCustomLoadout loadout) {
+        effect.currentDialog = new PCLCustomLoadoutEditEffect(title, loadout, effect.getBuilder().cardColor)
+                .addCallback(this::registerLoadout);
+    }
+
+    protected void openLoadoutDelete(PCLCustomLoadout loadout) {
+        if (loadout != null) {
+            effect.currentDialog = new PCLCustomDeletionConfirmationEffect<>(loadout)
+                    .addCallback((v) -> {
+                        if (v != null) {
+                            if (effect.getBuilder().loadout == loadout) {
+                                setLoadout(null);
+                            }
+                            v.info.wipe();
+                            refreshLoadoutItems();
+                        }
+                    });
+        }
+    }
+
+
     @Override
     public void refresh() {
         PCLDynamicRelicData builder = effect.getBuilder();
@@ -213,20 +269,59 @@ public class PCLCustomRelicPrimaryInfoPage extends PCLCustomGenericPage {
         effect.updateUpgradeEditorLimits(builder.maxUpgradeLevel);
     }
 
+    private void refreshLoadoutItems() {
+        loadoutDropdown.setItems(PCLLoadout.getAll(effect.currentSlot.slotColor));
+        loadoutDropdown.sortByLabel();
+        loadoutDropdown
+                .setActive(loadoutDropdown.size() > 0);
+        addLoadoutButton.setPosition(loadoutDropdown.getClearButtonHitbox().cX + loadoutDropdown.getClearButtonHitbox().width, loadoutDropdown.getClearButtonHitbox().cY)
+                .setActive(loadoutDropdown.isActive);
+        editLoadoutButton.setPosition(addLoadoutButton.hb.cX + addLoadoutButton.hb.width, addLoadoutButton.hb.cY);
+        deleteLoadoutButton.setPosition(editLoadoutButton.hb.cX + editLoadoutButton.hb.width, editLoadoutButton.hb.cY);
+    }
+
+    private void registerLoadout(PCLCustomLoadoutDialog dialog) {
+        if (dialog != null) {
+            if (dialog.loadout != null) {
+                dialog.loadout.languageMap = dialog.currentLanguageMap;
+                dialog.loadout.refreshStrings();
+                dialog.loadout.commitChanges();
+                effect.rebuildItem();
+                refreshLoadoutItems();
+            }
+            else {
+                PCLCustomLoadoutInfo info = new PCLCustomLoadoutInfo(dialog.currentID, EUIUtils.serialize(dialog.currentLanguageMap), effect.getBuilder().cardColor);
+                PCLCustomLoadoutInfo.register(info);
+                info.commit();
+                refreshLoadoutItems();
+            }
+        }
+    }
+
     @Override
     public void renderImpl(SpriteBatch sb) {
         header.tryRender(sb);
         idWarning.tryRender(sb);
         tierDropdown.tryRender(sb);
         sfxDropdown.tryRender(sb);
-        replacementDropdown.tryRender(sb);
         languageDropdown.tryRender(sb);
         nameInput.tryRender(sb);
         idInput.tryRender(sb);
+        loadoutDropdown.tryRender(sb);
+        addLoadoutButton.tryRender(sb);
+        editLoadoutButton.tryRender(sb);
+        deleteLoadoutButton.tryRender(sb);
+        replacementDropdown.tryRender(sb);
         maxUpgrades.tryRender(sb);
         branchUpgrades.tryRender(sb);
         loadoutValue.tryRender(sb);
         resetLoadoutValue.tryRender(sb);
+    }
+
+    protected void setLoadout(PCLLoadout loadout) {
+        effect.modifyAllBuilders((e, i) -> e.setLoadout(loadout));
+        editLoadoutButton.setActive(loadoutDropdown.isActive && loadout instanceof PCLCustomLoadout);
+        deleteLoadoutButton.setActive(editLoadoutButton.isActive);
     }
 
     @Override
@@ -236,6 +331,10 @@ public class PCLCustomRelicPrimaryInfoPage extends PCLCustomGenericPage {
         tierDropdown.tryUpdate();
         sfxDropdown.tryUpdate();
         replacementDropdown.tryUpdate();
+        loadoutDropdown.tryUpdate();
+        addLoadoutButton.tryUpdate();
+        editLoadoutButton.tryUpdate();
+        deleteLoadoutButton.tryUpdate();
         languageDropdown.tryUpdate();
         nameInput.tryUpdate();
         idInput.tryUpdate();
